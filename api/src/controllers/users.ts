ecf03e12438e704ef1290cc9226f0e3b7a4c17cd
Blaine Hill <blainehill2001@gmail.com>
2022-12-01 14:58:43 -0600

    removed /api

--- api/src/controllers/users.ts
+++
@@ -1,5 +0,0 @@
-import { catchErrors } from 'errors';
-
-export const getCurrentUser = catchErrors((req, res) => {
-  res.respond({ currentUser: req.currentUser });
-});
--- api/src/middleware/response.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandler } from 'express';
-
-export const addRespondToResponse: RequestHandler = (_req, res, next) => {
-  res.respond = (data): void => {
-    res.status(200).send(data);
-  };
-  next();
-};
