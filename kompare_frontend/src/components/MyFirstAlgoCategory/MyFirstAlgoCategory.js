7a1b40966323b587208a5388734fa47acbdea3a2
Blaine Hill <blainehill2001@gmail.com>
2023-01-17 13:13:21 -0600

    Fixed bad tab components

--- kompare_frontend/src/components/MyFirstAlgoCategory/MyFirstAlgoCategory.js
+++ kompare_frontend/src/components/MyFirstAlgoCategory/MyFirstAlgoCategory.js
@@ -41,7 +41,7 @@ const MyFirstAlgoCategory = () => {
       <div className="justify-center py-20">
         <div className="group relative mx-auto w-96 overflow-hidden bg-gray-300 rounded-[16px] p-[1px] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500">
           <div
-            className="relative rounded-[15px] p-6 text-purple-500"
+            className="relative rounded-[15px] p-6 text-purple-500 flex-auto"
             data-theme="mytheme"
           >
             <h5>MyFirstAlgoCategory Component</h5>
