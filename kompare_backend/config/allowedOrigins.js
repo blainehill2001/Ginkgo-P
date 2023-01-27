d5315ab00cbd120d568d0b049022bad329cac2f9
blainehill2001 <blainehill2001@gmail.com>
2023-01-27 17:11:37 -0600

    Added render frontend to allowed cors

--- kompare_backend/config/allowedOrigins.js
+++ kompare_backend/config/allowedOrigins.js
@@ -1,5 +1,8 @@
 //https://github.com/gitdagray/mern_stack_course/tree/main/lesson_13-backend/config
 
-const allowedOrigins = ["http://localhost:3000"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://kompare.onrender.com"
+];
 
 module.exports = allowedOrigins;
