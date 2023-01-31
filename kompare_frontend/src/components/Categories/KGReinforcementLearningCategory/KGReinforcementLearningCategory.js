d556ee81a9b78b94464cba5ba31d8b0941fe7b33
blainehill2001 <blainehill2001@gmail.com>
2023-01-31 03:03:24 -0600

    Fixed bad padding in home of each category

--- kompare_frontend/src/components/Categories/KGReinforcementLearningCategory/KGReinforcementLearningCategory.js
+++ kompare_frontend/src/components/Categories/KGReinforcementLearningCategory/KGReinforcementLearningCategory.js
@@ -30,7 +30,7 @@ const KGReinforcementLearningCategory = () => {
       <div className="justify-center py-20">
         <div className="group relative mx-auto w-6/12 overflow-hidden bg-gray-300 rounded-[16px] p-[1px] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500">
           <div
-            className="relative rounded-[15px] p-6 text-purple-500 flex-auto"
+            className="relative rounded-[15px] p-3 text-purple-500 flex-auto"
             data-theme="mytheme"
           >
             <h5>KGReinforcementLearningCategory Component</h5>
