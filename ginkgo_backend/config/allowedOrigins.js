a1e1c7bdb71216c810ab0dd83c4390e8bc5d8055
blainehill2001 <blainehill2001@gmail.com>
2023-02-03 00:12:12 -0600

    Changed cors to allow for "ginkgo-p"

--- ginkgo_backend/config/allowedOrigins.js
+++ ginkgo_backend/config/allowedOrigins.js
@@ -1,5 +1,8 @@
 //https://github.com/gitdagray/mern_stack_course/tree/main/lesson_13-backend/config
 
-const allowedOrigins = ["http://localhost:3000", "https://ginkgo.onrender.com"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://ginkgo-p.onrender.com"
+];
 
 module.exports = allowedOrigins;
