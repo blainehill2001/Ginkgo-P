6359f4b7296ad36beec2ac4e3316debca6890ca9
blainehill2001 <blainehill2001@gmail.com>
2023-12-14 01:03:46 -0600

    Added padding

--- ginkgo_frontend/src/components/Categories/KGReinforcementLearningCategory/KGReinforcementLearningCategory.js
+++ ginkgo_frontend/src/components/Categories/KGReinforcementLearningCategory/KGReinforcementLearningCategory.js
@@ -21,7 +21,7 @@ const KGReinforcementLearningCategory = () => {
   return (
     <>
       <div className="justify-center px-2">
-        <div className="group relative mx-auto w-6/12 overflow-hidden bg-gray-300 rounded-[16px] p-[1px] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500">
+        <div className="group relative mx-auto w-6/12 overflow-hidden bg-gray-300 rounded-[16px] p-[1px] py-0.5 transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500">
           <div
             className="relative rounded-[15px] text-purple-500 flex-auto p-5"
             data-theme="mytheme"
