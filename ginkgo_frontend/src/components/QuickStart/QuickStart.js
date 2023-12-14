0e7320cbadcf8e92b61b5b661bcdee4a35c99353
blainehill2001 <blainehill2001@gmail.com>
2023-12-14 00:29:32 -0600

    Added padding

--- ginkgo_frontend/src/components/QuickStart/QuickStart.js
+++ ginkgo_frontend/src/components/QuickStart/QuickStart.js
@@ -75,7 +75,7 @@ const QuickStart = () => {
   const queryValue = watch("query");
   return (
     <div className="justify-center py-20 px-80">
-      <div className="group relative mx-auto overflow-hidden bg-gray-300 rounded-[16px] p-[1px] transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500">
+      <div className="group relative mx-auto overflow-hidden bg-gray-300 rounded-[16px] p-[1px] py-0.5 transition-all duration-300 ease-in-out hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500">
         <div
           className="relative rounded-[15px] p-6 text-purple-500"
           data-theme="mytheme"
